6b6233c89325ca594e13d333285b15d6d169c86c
Michael Harper <mharper@foreflight.com>
2025-03-31 13:25:08 -0500

    Initial Commit

---
+++ TimePickerCrash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimePickerCrash
+//
+//  Created by Michael Harper on 3/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimePickerCrash/TimePickerCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimePickerCrashApp.swift
+//  TimePickerCrash
+//
+//  Created by Michael Harper on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct TimePickerCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
